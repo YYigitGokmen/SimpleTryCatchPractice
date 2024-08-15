587081ba4d531f32e59ec7a892b2d7609d01be58
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-08-15 16:55:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+public class Program
+{ 
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+                string userInput = Console.ReadLine();
+                     int squareRoot = Convert.ToInt32(userInput);
+
+             Console.WriteLine($"Girdiğiniz sayı: {squareRoot} karesi: {squareRoot * squareRoot}");
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
